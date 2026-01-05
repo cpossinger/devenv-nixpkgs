515665d33c65810d906e14f90c87830ba2566733
164182 <camden.possinger@delta.com>
2026-01-05 18:05:13 -0500

    remove rustdesk

---
+++ pkgs/development/python-modules/iamdata/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "iamdata";
+  version = "0.1.202512281";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "cloud-copilot";
+    repo = "iam-data-python";
+    tag = "v${version}";
+    hash = "sha256-gv3E+bIZiWqSBB5zXjSz5RicZY9aGY+EVtMbcUNpDNU=";
+  };
+
+  __darwinAllowLocalNetworking = true;
+
+  build-system = [ hatchling ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "iamdata" ];
+
+  enabledTestPaths = [ "iamdata/tests/*.py" ];
+
+  meta = {
+    description = "Module for utilizing AWS IAM data for Services, Actions, Resources, and Condition Keys";
+    homepage = "https://github.com/cloud-copilot/iam-data-python";
+    changelog = "https://github.com/cloud-copilot/iam-data-python/releases/tag/${src.tag}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ fab ];
+  };
+}
